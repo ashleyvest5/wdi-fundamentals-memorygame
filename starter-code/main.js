c7f5b2226134fb0045e4ee73a325ed49fba6efac
ashleyvest5 <ashleyvest@Marys-MacBook-Pro.local>
2017-01-23 16:14:27 -0800

    first edits to index.html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,20 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+cardOne == cardTwo
+cardThree == cardFour
+
+if (cardOne == cardTwo) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again."};
+}
+
+if (cardThree == cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again."};
+}
